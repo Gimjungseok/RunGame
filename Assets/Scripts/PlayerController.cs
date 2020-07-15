3acfd72b70262093244cfa5cbdfef7358dcb6a3a
Gimjungseok <63216469+Gimjungseok@users.noreply.github.com>
2020-07-15 10:00:49 +0900

    7월 15일

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -46,7 +46,7 @@ public class PlayerController : MonoBehaviour {
         playerAudio.Play();
         playerRigidbody.velocity = Vector2.zero;
         isDead = true;
-       // 사망 처리
+        GameManager.instance.OnPlayerDead();
    }
 
    private void OnTriggerEnter2D(Collider2D other) {
